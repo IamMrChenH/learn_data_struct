248801370dac1f8e4a0f88934cc2a2bca628d960
Lenovo <871761987@qq.com>
2019-04-16 11:35:03 +0800

    测试readme

---
+++ src/用队列实现栈.java
@@ -0,0 +1,53 @@
+//import java.util.LinkedList;
+//import java.util.Queue;
+//
+//public class 用队列实现栈 {
+//    Queue<Integer> mQueue1;
+//    Queue<Integer> mQueue2;
+//
+//    /**
+//     * Initialize your data structure here.
+//     */
+//    public 用队列实现栈() {
+//        mQueue1 = new LinkedList<>();//队列
+//        mQueue2 = new LinkedList<>();//反向
+//    }
+//
+//    /**
+//     * Push element x onto stack.
+//     */
+//    public void push(int x) {
+//
+//    }
+//
+//    /**
+//     * Removes the element on top of the stack and returns that element.
+//     */
+//    public int pop() {
+//
+//    }
+//
+//    /**
+//     * Get the top element.
+//     */
+//    public int top() {
+//
+//    }
+//
+//    /**
+//     * Returns whether the stack is empty.
+//     */
+//    public boolean empty() {
+//        return mQueue1.isEmpty() && mQueue2.isEmpty();
+//    }
+//
+///**
+// * Your MyStack object will be instantiated and called as such:
+// * MyStack obj = new MyStack();
+// * obj.push(x);
+// * int param_2 = obj.pop();
+// * int param_3 = obj.top();
+// * boolean param_4 = obj.empty();
+// */
+//
+//}
